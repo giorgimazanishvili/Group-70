c61dd93e6a058dbec1d98d256b84b2e1b5a5fa2a
giorgimazanishvili <1giorgimazanishvili@gmail.com>
2025-06-04 20:45:22 +0400

    next commit

---
+++ day 04/classwork.py/gigi2.py
@@ -0,0 +1,14 @@
+# 2) დაარქვით ამ ცვლადებს snake_case სინტაქსით
+
+# ხუთი ცვლადი snake_case სინტაქსით
+subject_name = "ლაშა"
+action = "მიყვარს"
+object_name = "პროგრამირება"
+time_of_action = "დღეს"
+place_of_action = "ბაღში"
+
+# წინადადება ცვლადების გამოყენებით
+sentence = f"{subject_name} {action} {object_name} {time_of_action} {place_of_action}."
+
+# წინადადების გამოყვანა
+print(sentence)
