25df49f3b7993227ca194486dbb586bf20caf389
giorgimazanishvili <1giorgimazanishvili@gmail.com>
2025-07-02 16:39:27 +0400

    next commit

---
+++ day 19/homework/hw3.py
@@ -0,0 +1,9 @@
+# 3)
+# სია:
+
+# words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+# აიღე სიის ბოლო 2 ელემენტი slicing-ის მეშვეობით.
+
+words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+list = words[4:]
+print(list)
---
+++ day 19/homework/hw5.py
@@ -0,0 +1,9 @@
+# 5)
+# სტრინგი:
+
+# text = "programming"
+# დაპრინტე მხოლოდ პირველი 5 სიმბოლო slicing-ის მეშვეობით.
+
+text = "programming"
+list = text[:5]
+print(list)
