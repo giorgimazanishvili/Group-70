4990ee4666d51f5c480da74aa1bc34799d358e83
giorgimazanishvili <1giorgimazanishvili@gmail.com>
2025-08-28 10:29:54 +0400

    next commit

---
+++ day 33/classwork/cw.py
@@ -0,0 +1,5 @@
+# 1) ჩამოთვალე განსხვავები box-ის მოდელებს შორის.
+
+# - Box model 1: Content, Padding, Border, Margin
+# - Box model 2: Content, Border, Margin
+# - Box model 3: Content, Padding, Margin
